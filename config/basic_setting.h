a46d0602f4c987b7c01a5a57d88fefe3b2abffee
HominSu <1774069959@qq.com>
2021-09-14 22:21:07 +0800

    增加推流相关代码，还未验证代码可行性

--- config/basic_setting.h
+++ config/basic_setting.h
@@ -14,27 +14,27 @@ namespace Basic {
 struct Setting {
  private:
   std::string uuid_;                // json: "uuid"
-  std::string server_rpc_address_;  // json: "server_rpc_address"
-  std::string server_rpc_port_;     // json: "server_rpc_port"
+  std::string rtmp_server_address_;  // json: "server_rpc_address"
+  std::string rtmp_server_port_;     // json: "server_rpc_port"
   std::string local_rpc_address_;   // json: "local_rpc_address"
   std::string local_rpc_port_;      // json: "local_rpc_port"
 
  public:
   explicit Setting(const nlohmann::json &j)
       : uuid_(j.at("uuid")),
-        server_rpc_address_(j.at("server_rpc_address")),
-        server_rpc_port_(j.at("server_rpc_port")),
+        server_rpc_address_(j.at("rtmp_server_address")),
+        server_rpc_port_(j.at("rtmp_server_port")),
         local_rpc_address_(j.at("local_rpc_address")),
         local_rpc_port_(j.at("local_rpc_port")) {};
 
   [[nodiscard]] const std::string &uuid() const {
     return uuid_;
   }
-  [[nodiscard]] const std::string &server_rpc_address() const {
-    return server_rpc_address_;
+  [[nodiscard]] const std::string &rtmp_server_address() const {
+    return rtmp_server_address_;
   }
-  [[nodiscard]] const std::string &server_rpc_port() const {
-    return server_rpc_port_;
+  [[nodiscard]] const std::string &rtmp_server_port() const {
+    return rtmp_server_port_;
   }
   [[nodiscard]] const std::string &local_rpc_address() const {
     return local_rpc_address_;
